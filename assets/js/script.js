68496f02733ddce29f810a2434e468635a5e2f27
liyuan23 <quekliyuan@gmail.com>
2017-11-28 10:58:31 +0800

    added v-ifs to hide unknowns/none/n/a and undefineds

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,19 +24,54 @@ var starWC = new Vue({
         url: 'https://swapi.co/api/people/1/',
         image: '/assets/images/luke.jpg',
         backgroundImage: '/assets/images/luke1.jpg'
-      }
+      },
+      {
+        url: 'https://swapi.co/api/people/3/',
+        image: '/assets/images/luke.jpg',
+        backgroundImage: '/assets/images/luke1.jpg'
+      },
+      {
+        url: 'https://swapi.co/api/people/5/',
+        image: '/assets/images/luke.jpg',
+        backgroundImage: '/assets/images/luke1.jpg'
+      },
+      {
+        url: 'https://swapi.co/api/people/13/',
+        image: '/assets/images/luke.jpg',
+        backgroundImage: '/assets/images/luke1.jpg'
+      },
+      {
+        url: 'https://swapi.co/api/people/14/',
+        image: '/assets/images/luke.jpg',
+        backgroundImage: '/assets/images/luke1.jpg'
+      },
+      {
+        url: 'https://swapi.co/api/people/27/',
+        image: '/assets/images/luke.jpg',
+        backgroundImage: '/assets/images/luke1.jpg'
+      },
       // {
-      //   url: 'https://swapi.co/api/people/3/',
-      //   image: '/assets/images/luke.jpg',
-      //   backgroundImage: '/assets/images/luke1.jpg'
-      // }
-      // {
-      //   url: 'https://swapi.co/api/people/5/',
+      //   url: 'https://swapi.co/api/people/84/',
       //   image: '/assets/images/luke.jpg',
       //   backgroundImage: '/assets/images/luke1.jpg'
       // },
       // {
-      //   url: 'https://swapi.co/api/people/13/',
+      //   url: 'https://swapi.co/api/people/85/',
+      //   image: '/assets/images/luke.jpg',
+      //   backgroundImage: '/assets/images/luke1.jpg'
+      // },
+      // {
+      //   url: 'https://swapi.co/api/people/86/',
+      //   image: '/assets/images/luke.jpg',
+      //   backgroundImage: '/assets/images/luke1.jpg'
+      // },
+      // {
+      //   url: 'https://swapi.co/api/people/87/',
+      //   image: '/assets/images/luke.jpg',
+      //   backgroundImage: '/assets/images/luke1.jpg'
+      // },
+      // {
+      //   url: 'https://swapi.co/api/people/88/',
       //   image: '/assets/images/luke.jpg',
       //   backgroundImage: '/assets/images/luke1.jpg'
       // }
@@ -91,70 +126,68 @@ var starWC = new Vue({
         this.results.starship2 = starship2
       })
     })
-      // if n/a then hide
       // convert all first char to uppercase
-
-
-// USING FETCH CALL API RANDOMISED
-    // // fetching list of characters and storing
-    // fetch(url, options)
-    // .then(response => {
-      // return response.json()
-    // })
-    // .then(data => {
-    //   // store list of characters
-    //   characters = data.characters
-    //   // get length of characters array
-    //   let len = characters.length
-    //   // get a random index
-    //   let index = Math.floor(Math.random() * len)
-    //   // get a random character
-    //   randomCharUrl = characters[index]
-    // })
-    // .then(() => {
-    //   // fetching character object
-    //   fetch(randomCharUrl, options)
-    //   .then(response => {
-    //     return response.json()
-    //   })
-    //   .then(data => {
-    //     characterObj = data
-    //     console.log(characterObj)
-    //     this.results = characterObj
-    //   })
-    // })
-
-// USING AXIOS CALL API RANDOMISED, rmb to add in axios in html
-    // axios.get("https://swapi.co/api/films/7/")
-    // .then(response => {
-    //   // console.log(response.data.characters)
-    //   characters = response.data.characters
-    //   // console.log(characters)
-    //   randomChar = characters[Math.floor(Math.random() * characters.length)]
-    //   // console.log(randomChar)
-    // })
-    // .then(() => {
-    //   console.log('characters: ', characters)
-    // })
-    // .then(data => {
-    //   axios.get(randomChar)
-    //   .then(response => {
-    //     this.results = response.data
-    //     species = this.results.species[0]
-    //     this.results.species = species
-    //     console.log('species: ', species)
-    //
-    //     fetch(species)
-    //     .then(response => response.json())
-    //     .then(species => {
-    //       name = species.name
-    //       classification = species.classification
-    //       language = species.language
-    //       console.log('species: ', species)
-    //       console.log(name, classification, language)
-    //       this.results.speciesName = name
-    //     })
-    //   })
-    // })
   }
 })
+
+// USING FETCH CALL API RANDOMISED
+// // fetching list of characters and storing
+// fetch(url, options)
+// .then(response => {
+// return response.json()
+// })
+// .then(data => {
+//   // store list of characters
+//   characters = data.characters
+//   // get length of characters array
+//   let len = characters.length
+//   // get a random index
+//   let index = Math.floor(Math.random() * len)
+//   // get a random character
+//   randomCharUrl = characters[index]
+// })
+// .then(() => {
+//   // fetching character object
+//   fetch(randomCharUrl, options)
+//   .then(response => {
+//     return response.json()
+//   })
+//   .then(data => {
+//     characterObj = data
+//     console.log(characterObj)
+//     this.results = characterObj
+//   })
+// })
+
+// USING AXIOS CALL API RANDOMISED, rmb to add in axios in html
+// axios.get("https://swapi.co/api/films/7/")
+// .then(response => {
+//   // console.log(response.data.characters)
+//   characters = response.data.characters
+//   // console.log(characters)
+//   randomChar = characters[Math.floor(Math.random() * characters.length)]
+//   // console.log(randomChar)
+// })
+// .then(() => {
+//   console.log('characters: ', characters)
+// })
+// .then(data => {
+//   axios.get(randomChar)
+//   .then(response => {
+//     this.results = response.data
+//     species = this.results.species[0]
+//     this.results.species = species
+//     console.log('species: ', species)
+//
+//     fetch(species)
+//     .then(response => response.json())
+//     .then(species => {
+//       name = species.name
+//       classification = species.classification
+//       language = species.language
+//       console.log('species: ', species)
+//       console.log(name, classification, language)
+//       this.results.speciesName = name
+//     })
+//   })
+// })
